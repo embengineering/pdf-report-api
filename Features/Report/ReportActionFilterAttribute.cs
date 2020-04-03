87bacc943f540bb4b01efaf54af37cf8eefc2e81
Emmanuel Morales <emmanuel.morales@headspring.com>
2020-04-03 15:15:59 -0500

    Add header and footer to report

--- Features/Report/ReportActionFilterAttribute.cs
+++ Features/Report/ReportActionFilterAttribute.cs
@@ -22,15 +22,20 @@ namespace PdfReport.Api.Features.Report
                 return;
             }
 
-            var html = await contextController.RenderViewAsync(model);
-
-            resultContext.Result = ConvertHtmlToPdf(contextController, model, html);
+            resultContext.Result = await GeneratePdf(contextController, model);
         }
 
-        private FileResult ConvertHtmlToPdf(Controller contextController, SampleReport model, string html)
+        private async Task<FileResult> GeneratePdf(Controller contextController, SampleReport model)
         {
+            var baseUrl = $"{contextController.Request.Scheme}://{contextController.Request.Host}{contextController.Request.PathBase}";
+
             // Create a HTML to PDF converter object with default settings
             var htmlToPdfConverter = new HtmlToPdfConverter();
+
+            htmlToPdfConverter.PdfDocumentOptions.TopMargin = 8;
+            htmlToPdfConverter.PdfDocumentOptions.BottomMargin = 8;
+            htmlToPdfConverter.PdfDocumentOptions.LeftMargin = 8;
+            htmlToPdfConverter.PdfDocumentOptions.RightMargin = 8;
 
             // Set HTML Viewer width in pixels which is the equivalent in converter of the browser window width
             // htmlToPdfConverter.HtmlViewerWidth = int.Parse(model.HtmlViewerWidth);
@@ -57,9 +62,15 @@ namespace PdfReport.Api.Features.Report
             // if (model.ConversionDelay > 0)
             //     htmlToPdfConverter.ConversionDelay = model.ConversionDelay;
 
+            htmlToPdfConverter.PdfDocumentOptions.ShowHeader = true;
+            await SetHeader(htmlToPdfConverter, contextController, model.Header, baseUrl); 
+            
+            htmlToPdfConverter.PdfDocumentOptions.ShowFooter = true;
+            await SetFooter(htmlToPdfConverter, contextController, model.Footer, baseUrl);
+
             // Convert the HTML page given by an URL to a PDF document in a memory buffer
-            var baseUrl = $"{contextController.Request.Scheme}://{contextController.Request.Host}{contextController.Request.PathBase}";
-            var outPdfBuffer = htmlToPdfConverter.ConvertHtml(html, baseUrl);
+            var htmlBody = await contextController.RenderViewAsync(model);
+            var outPdfBuffer = htmlToPdfConverter.ConvertHtml(htmlBody, baseUrl);
 
             // Send the PDF file to browser
             FileResult fileResult = new FileContentResult(outPdfBuffer, "application/pdf")
@@ -70,58 +81,94 @@ namespace PdfReport.Api.Features.Report
             return fileResult;
         }
 
-        // private async Task<HtmlToPdf> GetConfiguredHtmlToPdfRendererAsync(Controller contextController, SampleReport model)
-        // {
-        //     var renderer = new HtmlToPdf
-        //     {
-        //         PrintOptions =
-        //         {
-        //             PaperOrientation = PdfPrintOptions.PdfPaperOrientation.Landscape,
-        //             MarginBottom = 8,
-        //             MarginTop = 8,
-        //             MarginLeft = 8,
-        //             MarginRight = 8,
-        //             Header = await RenderHeaderAsync(contextController, model.Header),
-        //             Footer = await RenderFooterAsync(contextController, model.Footer),
-        //             FitToPaperWidth = true,
-        //             Title = model.Header.ReportTitle
-        //         }
-        //     };
-        //
-        //     renderer.PrintOptions.FitToPaperWidth = true;
-        //
-        //     return renderer;
-        // }
-        //
-        private static string GetReportName(ReportHeader modelHeader, string ext)
+        private async Task SetHeader(HtmlToPdfConverter htmlToPdfConverter, Controller contextController, ReportHeader header, string baseUrl)
+        {
+            // Set the header height in points
+            htmlToPdfConverter.PdfHeaderOptions.HeaderHeight = 60;
+
+            // Set header background color
+            htmlToPdfConverter.PdfHeaderOptions.HeaderBackColor = RgbColor.White;
+
+            // Create a HTML element to be added in header
+            var headerHtml = await contextController.RenderViewAsync("_SampleReportHeader", header);
+            var headerElement = new HtmlToPdfElement(headerHtml, baseUrl);
+
+            // Set the HTML element to fit the container height
+            headerElement.FitHeight = true;
+
+            // Add HTML element to header
+            htmlToPdfConverter.PdfHeaderOptions.AddElement(headerElement);
+
+            // Calculate the header width based on PDF page size and margins
+            var headerWidth = htmlToPdfConverter.PdfDocumentOptions.PdfPageSize.Height -
+                              htmlToPdfConverter.PdfDocumentOptions.LeftMargin -
+                              htmlToPdfConverter.PdfDocumentOptions.RightMargin;
+
+            // Calculate header height
+            var headerHeight = htmlToPdfConverter.PdfHeaderOptions.HeaderHeight;
+
+            // Create a line element for the bottom of the header
+            var headerLine = new LineElement(htmlToPdfConverter.PdfDocumentOptions.LeftMargin, headerHeight - 1, headerWidth, headerHeight - 1);
+
+            // Set line color
+            headerLine.ForeColor = RgbColor.Gray;
+
+            // Add line element to the bottom of the header
+            htmlToPdfConverter.PdfHeaderOptions.AddElement(headerLine);
+        }
+
+        private async Task SetFooter(HtmlToPdfConverter htmlToPdfConverter, Controller contextController, ReportFooter footer, string baseUrl)
+        {
+            // Set the footer height in points
+            htmlToPdfConverter.PdfFooterOptions.FooterHeight = 60;
+
+            // Set footer background color
+            htmlToPdfConverter.PdfFooterOptions.FooterBackColor = RgbColor.White;
+
+            // Create a HTML element to be added in footer
+            var footerHtml = await contextController.RenderViewAsync("_SampleReportFooter", footer);
+            var footerElement = new HtmlToPdfElement(footerHtml, baseUrl);
+
+            // Set the HTML element to fit the container height
+            footerElement.FitHeight = true;
+
+            // Add HTML element to footer
+            htmlToPdfConverter.PdfFooterOptions.AddElement(footerElement);
+
+            // Add page numbering
+            // Create a text element with page numbering place holders &p; and & P;
+            var footerText = new TextElement(0, 30, "Page &p; of &P;  ",
+                new PdfFont("Consolas, Monospace", 9, false));
+
+            // Align the text at the right of the footer
+            footerText.TextAlign = HorizontalTextAlign.Right;
+
+            // Set page numbering text color
+            footerText.ForeColor = RgbColor.Black;
+
+            // Embed the text element font in PDF
+            footerText.EmbedSysFont = true;
+
+            // Add the text element to footer
+            htmlToPdfConverter.PdfFooterOptions.AddElement(footerText);
+
+            // Calculate the footer width based on PDF page size and margins
+            var footerWidth = htmlToPdfConverter.PdfDocumentOptions.PdfPageSize.Height -
+                              htmlToPdfConverter.PdfDocumentOptions.LeftMargin - htmlToPdfConverter.PdfDocumentOptions.RightMargin;
+
+            // Create a line element for the top of the footer
+            var footerLine = new LineElement(htmlToPdfConverter.PdfDocumentOptions.LeftMargin, 0, footerWidth, 0);
+
+            // Set line color
+            footerLine.ForeColor = RgbColor.Gray;
+
+            // Add line element to the bottom of the footer
+            htmlToPdfConverter.PdfFooterOptions.AddElement(footerLine);
+        }
+
+        private string GetReportName(ReportHeader modelHeader, string ext)
         {
             return $"Report_{modelHeader.StartDate::yyyyMMdd}_{modelHeader.EndDate::yyyyMMdd}.{ext}";
         }
-        //
-        // private static async Task<HtmlHeaderFooter> RenderHeaderAsync(Controller contextController, ReportHeader header)
-        // {
-        //     var html = await contextController.RenderViewAsync("_SampleReportHeader", header);
-        //     return new HtmlHeaderFooter
-        //     {
-        //         Height = 20,
-        //         HtmlFragment = html,
-        //         DrawDividerLine = true,
-        //         Spacing = 5,
-        //     };
-        // }
-        //
-        // private static async Task<HtmlHeaderFooter> RenderFooterAsync(Controller contextController, ReportFooter footer)
-        // {
-        //     var pageCount = "{page} of {total-pages}";
-        //     var html = await contextController.RenderViewAsync("_SampleReportFooter", footer);
-        //
-        //     return new HtmlHeaderFooter
-        //     {
-        //         Height = 20,
-        //         HtmlFragment = html.Replace("TEMP_PLACE_HOLDER_PAGE_COUNT", pageCount),
-        //         DrawDividerLine = true,
-        //         Spacing = 5,
-        //     };
-        // }
     }
 }
